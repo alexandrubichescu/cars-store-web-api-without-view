9e0db21828a39c8118bea2969b054356365bbeee
Alexandru <117373448+alexandrubichescu@users.noreply.github.com>
2023-02-22 21:49:12 +0200

    Update Program.cs

--- CarsStore/Program.cs
+++ CarsStore/Program.cs
@@ -3,19 +3,15 @@ using CarsStore.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-//IConfiguration configuration = builder.Configuration;
 // Add services to the container.
-//builder.Services.AddDbContext<CarStoreContext>(options =>options.UseSqlServer(builder.GetConnectionString("CarsStoreDB")));
 builder.Services.AddControllers().AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<ICarRepository, CarRepository>();
+
 builder.Services.AddDbContext<CarStoreContext>(options =>
 {
-
     options.UseSqlServer(builder.Configuration["ConnectionStrings:CarsStoreDBConnection"]);
 });
 
